87e3881bb114c20e57992faf855760cbb6e24924
Andycillin <andy.tran11996@gmail.com>
2017-11-13 23:17:05 +0100

    init SSD

---
+++ ssd.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+
+from torch.autograd import Variable
+
+
+class SSD(nn.Module):
+    def __init__(self):
+        super(SSD, self).__init__()
+
+    def forward(self, input):
+        return []
